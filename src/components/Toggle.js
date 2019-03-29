c7596d82485ec2a0533f4b658ae04b57897d2d8a
hibak100 <hibak_100@hotmail.co.uk>
2019-03-29 15:56:44 +0000

    highlight border when toggle button is clicked

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -6,14 +6,14 @@ class Toggle extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      value: "t1",
+      value: "t1"
     };
   }
 
   handleChange = event => {
     const value =  event.target.id;
     if(value !== this.state.value) {
-      this.setState({value});
+      this.setState({ value });
       this.props.toggleClick(value);
     }
   };
@@ -21,8 +21,8 @@ class Toggle extends Component {
   render() {
     return (
       <div className="Toggle">
-      <button id={"t1"}   className="Button" onClick={this.handleChange}> Table 1 </button>
-      <button id={"both"} className="Button" onClick={this.handleChange}> Both Tables </button>
+      <button id={"t1"}   className={`Button ${this.state.value === "t1" && 'Active'}`} onClick={this.handleChange}> Table 1 </button>
+      <button id={"both"} className={`Button ${this.state.value === "both" && 'Active'}`} onClick={this.handleChange}> Both Tables </button>
       </div>
     );
   }
@@ -31,8 +31,6 @@ class Toggle extends Component {
 Toggle.propTypes = {
   toggleClick: PropTypes.func
 };
-
-
 
 export default Toggle;
 
